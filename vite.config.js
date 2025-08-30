5ea1d76865446ba0aa5800fe0efb0d53eacf241e
unknown <01026676@r.recruit.co.jp>
2025-08-30 11:20:43 +0900

    Fix asset paths for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ← リポジトリ名と完全一致させる
-  //base: '/beauteos-user-mock-Ver1/',
+  // ★★★ ここが一番重要です ★★★
+  // GitHub Pagesで公開するリポジトリ名を指定します
+  base: "/beauteos-user-mock-Ver2/", 
 })
